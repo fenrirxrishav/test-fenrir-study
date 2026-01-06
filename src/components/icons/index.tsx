d69bac9b4f0af732bb973e9500208dbf259dbc84
Ankit Kumar <ankitkrpt@gmail.com>
2026-01-06 08:07:09 +0000

    v5.6

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,5 +1,4 @@
-
-export function FenrirLogo({className}: {className?: string}) {
+export function FenrirLogo({ className }: { className?: string }) {
     return (
         <svg
             className={className}
@@ -10,13 +9,12 @@ export function FenrirLogo({className}: {className?: string}) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <path d="M14 12l-3.43 3.43a2 2 0 0 0 0 2.83l.57.57a2 2 0 0 0 2.83 0L17 16" />
-            <path d="M19 19 12 12" />
-            <path d="m14 7 3 3" />
-            <path d="M10 12 7.17 9.17a2 2 0 0 0-2.83 0L4 9.5a2 2 0 0 0 0 2.83L6.83 15" />
-            <path d="m12 12 5.17 5.17a2 2 0 0 0 2.83 0L20.5 16a2 2 0 0 0 0-2.83L17.66 10.34" />
-            <path d="M14.24 9.47s.5-2.47 2.24-4.24" />
-            <path d="M10.15 13.85s-2.47.5-4.24 2.24" />
+            <path d="M3 6l5-3 4 2 4-2 5 3" />
+            <path d="M8 3l1.5 4L12 6l2.5 1L16 3" />
+            <path d="M3 6l2 9 7 6 7-6 2-9" />
+            <path d="M9 14h.01" />
+            <path d="M15 14h.01" />
+            <path d="M10 17l2 1 2-1" />
         </svg>
     );
 }
